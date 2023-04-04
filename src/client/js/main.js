1bbfab1150c3368e01ea6fc88b5f13739a2f7b93
Haebi <haebi@users.noreply.github.com>
2023-04-04 22:22:18 +0900

    music chart
    music chart
    + backend
    + frontend
    + basic css

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,52 @@
-import "../scss/styles.scss";
+//import "../scss/styles.scss";
 
 console.log("hi");
+//alert('hi');
+
+
+function _makeChartRow(rank, title, played)
+{
+    const divRow = document.createElement("div");
+    const divCol1 = document.createElement("div");
+    const divCol2 = document.createElement("div");
+    const divCol3 = document.createElement("div");
+    const divCol1Span = document.createElement("span");
+    const divCol2Span = document.createElement("span");
+    const divCol3Span = document.createElement("span");
+
+
+    divCol1Span.innerText = rank;
+    divCol2Span.innerText = title;
+    divCol3Span.innerText = played;
+
+    divCol1.appendChild(divCol1Span);
+    divCol2.appendChild(divCol2Span);
+    divCol3.appendChild(divCol3Span);
+
+    divRow.appendChild(divCol1);
+    divRow.appendChild(divCol2);
+    divRow.appendChild(divCol3);
+
+    return divRow;
+}
+
+function getMusicChart()
+{
+    const mc = document.querySelector("#musicChart");
+    mc.appendChild(_makeChartRow('Rank', 'Title', 'Played'));
+
+    const url = `http://localhost:4000/top10`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            for (let i = 0; i < data.length; i++) {
+                mc.appendChild(_makeChartRow(i+1, data[i].title, data[i].played));
+            }
+        });    
+}
+
+getMusicChart();
+
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import rootRouter from "./routers/rootRouter";
+import mongoose from "mongoose";
 
 const app = express();
 const logger = morgan("dev");
@@ -25,11 +26,93 @@ app.use(
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("assets"));
+//app.use("/static", express.static("assets"));
+app.use("/static", express.static(__dirname + "/client"));
 
 app.use("/", rootRouter);
 /*
 Add more routers here!
 */
+
+// MongoDB 연결
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const songSchema = new mongoose.Schema({
+  title: { type: String, unique: true },
+  played: Number,
+},{versionKey : false});
+
+const Song = mongoose.model('song', songSchema);
+
+app.get("/top10", (req, res) => {
+  Song.find({}).sort({ played: -1 }).limit(10).exec((err, songs) => {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      //console.log('Top 10 played songs:', songs);
+      const jsonSongs = JSON.stringify(songs);
+      console.log(jsonSongs);
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+      res.setHeader('Content-Type', 'application/json');
+      res.send(jsonSongs);
+    }
+  });
+});
+
+// sample data insert
+app.get("/insert_sample", (req, res) => {
+  // 새로운 모델 생성
+  // const songSchema = new mongoose.Schema({
+  //   title: { type: String, unique: true },
+  //   played: Number,
+  // },{versionKey : false});
+
+  // const Song = mongoose.model('song', songSchema);
+
+  const sampleList = [
+    {
+      title: 'FAKE LOVE - BTS',
+      played: 44,
+    },
+    {
+      title: '달려라 방탄 - BTS',
+      played: 22,
+    },
+    {
+      title: 'Permission to Dance - BTS',
+      played: 33,
+    },
+    {
+      title: 'Butter - BTS',
+      played: 11,
+    },
+    {
+      title: 'Dynamite - BTS',
+      played: 55,
+    },
+  ];
+
+  for (let i = 0; i < sampleList.length; i++) {
+    // 새로운 데이터 저장
+    const newSong = new Song({
+      title: sampleList[i].title,
+      played: sampleList[i].played,
+    });
+
+    newSong.save((err, savedSong) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Saved song:', savedSong);
+      }
+    });
+  }
+
+  res.send('sample data stored.');
+});
 
 export default app;
